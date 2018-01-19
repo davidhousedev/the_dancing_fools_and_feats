c94bea2eec84452258b24b32e8759ffe987d34d9
David House <davidhousedev@gmail.com>
2018-01-18 22:24:49 -0500

    devops: Automate creation of different image sizes from source images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'), // gulp package
     sass = require('gulp-sass'), // sass compiler
     cssmin = require('gulp-clean-css'), // minify css
+    imageResize = require('gulp-image-resize'), // resize images
     path = require('path'), // node.js module that serve path
     rename = require("gulp-rename"), //rename module
     autoprefixer = require('gulp-autoprefixer'); //prefixes for cross-browser compatibility
@@ -24,9 +25,45 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('.')); //write compilation result
 });
 
+
+const img_sizes = {
+    xs: 200,
+    sm: 400,
+    md: 800,
+    lg: 1200,
+    xlg: 2000
+};
+
+var imgResizeTasks = [];
+for (var size in img_sizes) {
+    if (!img_sizes.hasOwnProperty(size)) {
+        continue
+    }
+    var taskName = 'resize_' + size;
+    function createTask(taskName, img_size, width) {
+        gulp.task(taskName, function() {
+            var src = path.join(rootPath, './static', '**', 'img/src');
+            return gulp.src(path.join(src, '*.jpg'), {base: '.'})
+                .pipe(imageResize({
+                    width: width,
+                    crop: false
+                }))
+                .pipe(rename(function (path) {
+                    path.dirname = path.dirname.replace('/src', '/dist/' + img_size);
+                    path.extname = '.jpg';
+                }))
+                .pipe(gulp.dest('.'))
+        });
+    }
+    createTask(taskName, size, img_sizes[size]);
+    imgResizeTasks.push(taskName);
+}
+
+
 gulp.task('watch', function () { // watch for changes in all scss files
     var src = path.join(rootPath, '**', '*.scss');
     gulp.watch(src, ['sass']); //recompile all files on scss change
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('imgs', imgResizeTasks);
+gulp.task('default', ['sass', 'imgs', 'watch']);
