e2eab7b4f6c55b17788c2d6c761d094f2b3dedb1
David House <davidhousedev@gmail.com>
2018-02-12 22:36:23 -0500

    feat: Create page for Weekly Classes and Social Dancing

--- the_dancing_fools_and_feats/fools/urls.py
+++ the_dancing_fools_and_feats/fools/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('classes_and_dance', views.ClassAndDanceView.as_view()),
     path('', views.IndexView.as_view())
 ]
--- the_dancing_fools_and_feats/fools/views.py
+++ the_dancing_fools_and_feats/fools/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'fools/index.html'
+
+class ClassAndDanceView(TemplateView):
+    template_name = 'fools/classes_and_dance.html'
