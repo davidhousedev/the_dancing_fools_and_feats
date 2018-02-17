2f2fa4acf29b379c89f71609814aaa7c4899a12f
David House <davidhousedev@gmail.com>
2018-02-17 15:56:09 -0500

    refactor: Organize templates in subdirectories

--- the_dancing_fools_and_feats/fools/views.py
+++ the_dancing_fools_and_feats/fools/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'fools/index.html'
+    template_name = 'fools/pages/index.html'
 
 class ClassAndDanceView(TemplateView):
-    template_name = 'fools/classes_and_dance.html'
+    template_name = 'fools/pages/classes_and_dance.html'
