35d2ab92450d28f724ec2e79972457720e63bd4f
David House <davidhousedev@gmail.com>
2018-02-17 00:26:27 -0500

    feat: Create links between index and classes and dance pages

--- the_dancing_fools_and_feats/fools/urls.py
+++ the_dancing_fools_and_feats/fools/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('classes_and_dance', views.ClassAndDanceView.as_view()),
-    path('', views.IndexView.as_view())
+    path('classes_and_dance',
+         views.ClassAndDanceView.as_view(),
+         name='fools-classes-dance'),
+    path('',
+         views.IndexView.as_view(),
+         name='fools-index')
 ]
