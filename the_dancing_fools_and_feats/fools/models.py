6e291c9bcca6d9406e5c9fc9200640e2f8e6ea8a
David House <davidhousedev@gmail.com>
2018-03-10 12:26:02 -0500

    feat: Add help text to database admin view

--- the_dancing_fools_and_feats/fools/models.py
+++ the_dancing_fools_and_feats/fools/models.py
@@ -5,6 +5,7 @@ from django.db import models
 EVENT_MAX_TITLE_LENGTH = 100
 EVENT_DESCRIPTION_MAX_LENGTH = 300
 EVENT_LOCATION_MAX_LENGTH = 60
+EVENT_URL_MAX_LENGTH = 300
 ONE_WEEK = 7
 ONE_MONTH = 30
 ONE_YEAR = 365
@@ -16,19 +17,24 @@ class DanceEvent(models.Model):
 
     title = models.CharField(
         max_length=EVENT_MAX_TITLE_LENGTH,
-        help_text='User-facing name of the event')
+        help_text=f'User-facing name of the event. '
+                  f'max {EVENT_MAX_TITLE_LENGTH} characters')
     occurrence = models.DateField(
         help_text='Example date of this event, used with frequency to '
-                  'anticipate when this event will occur in the future.')
+                  'anticipate when this event will occur in the future. '
+                  'format: YYYY-MM-DD')
     website_url = models.URLField(
-        max_length=300,
-        help_text='Official website of this event')
+        max_length=EVENT_URL_MAX_LENGTH,
+        help_text=f'Official website of this event. '
+                  f'max: {EVENT_URL_MAX_LENGTH} characters')
     location = models.CharField(
         max_length=EVENT_LOCATION_MAX_LENGTH,
-        help_text="Approximate location of this event, e.g. Newton, MA")
+        help_text=f'Approximate location of this event, e.g. Newton, MA. '
+                  f'max: {EVENT_LOCATION_MAX_LENGTH} characters')
     description = models.TextField(
         max_length=EVENT_DESCRIPTION_MAX_LENGTH,
-        help_text='User-facing description of this event')
+        help_text=f'User-facing description of this event. '
+                  f'max: {EVENT_DESCRIPTION_MAX_LENGTH} characters')
     order = models.IntegerField(
         help_text='Visual order of the event compared to other events. '
                   'Lower numbers will appear atop other events.')
